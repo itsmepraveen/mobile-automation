b02d645fd0baad2384c9f523f44d75006744edd5
Praveenkumar <praveen.radhakrishnan@woolworths.com.au>
2021-01-25 22:23:44 +1100

    framework setup - wip

--- src/main/java/au/com/wow/WooliesAndroid/Base.java
+++ src/main/java/au/com/wow/WooliesAndroid/Base.java
@@ -1,5 +1,9 @@
 package au.com.wow.WooliesAndroid;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
 public class Base {
 
+	AndroidDriver<AndroidElement> driver;
 }
